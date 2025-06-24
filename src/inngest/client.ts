34c5eeaf47b1175aa8d64ccf75b42bc7b93396ac
mendsalbert <mendsalbert@gmail.com>
2025-06-24 14:54:27 +0000

    eventkey

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -4,7 +4,8 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "ai-therapy-agent",
   // You can add your Inngest signing key here if you have one
-  // signingKey: process.env.INNGEST_SIGNING_KEY,
+  eventKey:
+    "Smzg-08plP_p4P1T9R6UA00_gbW4eDi8B-Ay_F6DCB5OYIWqHcAMbGLNRVgLCCJDS3YFth0D3_P3FzPO4vpl0w",
 });
 
 // Export the functions array (this will be populated by the functions.ts file)
